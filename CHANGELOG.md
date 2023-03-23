c467a3ce87d891c5c562f4975d06c673c6c796e8
Felix Brucker <felix@felixbrucker.com>
2023-03-23 19:33:15 +0100

    Bump to 1.2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Changelog
 
-## [Unreleased]
+## [1.2.0] - 2023-03-23
 
 ### Added
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='foxy-farmer',
-    version='1.1.0',
+    version='1.2.0',
     url='https://foxypool.io',
     license='GPLv3',
     author='Felix Brucker',
