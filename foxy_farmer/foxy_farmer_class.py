a364d925cc021dbc85aea1821a56daec611a5d57
Felix Brucker <felix@felixbrucker.com>
2024-02-01 17:15:05 +0700

    Use ctrl handler on windows to handle x clicks

--- foxy_farmer/foxy_farmer_class.py
+++ foxy_farmer/foxy_farmer_class.py
@@ -1,7 +1,8 @@
-from asyncio import AbstractEventLoop
+from asyncio import AbstractEventLoop, new_event_loop
 from logging import Logger, getLogger
 from pathlib import Path
 from signal import Signals
+from sys import platform
 from types import FrameType
 from typing import Optional
 
@@ -60,15 +61,28 @@ class FoxyFarmer:
         with auto_session_tracking(session_mode="application"):
             await self._farmer.run()
 
+    async def stop(self) -> None:
+        if self._farmer is not None:
+            await self._farmer.stop()
+
     async def _accept_signal(
             self,
             signal_: Signals,
             stack_frame: Optional[FrameType],
             loop: AbstractEventLoop,
     ) -> None:
-        if self._farmer is not None:
-            await self._farmer.stop()
+        await self.stop()
 
     async def setup_process_global_state(self) -> None:
-        async with SignalHandlers.manage() as signal_handlers:
-            signal_handlers.setup_async_signal_handler(handler=self._accept_signal)
+        if platform == "win32" or platform == "cygwin":
+            from win32api import SetConsoleCtrlHandler
+
+            def on_exit(sig, func=None):
+                new_loop = new_event_loop()
+                new_loop.run_until_complete(new_loop.create_task(self.stop()))
+                new_loop.stop()
+
+            SetConsoleCtrlHandler(on_exit, True)
+        else:
+            async with SignalHandlers.manage() as signal_handlers:
+                signal_handlers.setup_async_signal_handler(handler=self._accept_signal)
